caced6ee6c8f01e5cb395a31c7b1c6655dfe6fb6
Matt Gebert <mattheuu.gebert@gmail.com>
2016-12-26 00:20:13 +1100

    Beginning of D3 Integration

--- home.js
+++ home.js
@@ -1,4 +1,14 @@
-var data = [4,8,15,16,23,42];
-var back = d3.select("background");
-var div = back.append("div");
-div.html("Hello World");
+// Testing d3.js scripting!
+
+var data = [4,8,15,18,23,42];
+
+
+
+
+
+//--------------DIV Graph Generation!-----------------
+// d3.select(".chart")
+//   .selectAll("div")
+//     .data(data)
+//   .enter().append("div").style("width", function(d) { return d * 10 + "px"; })
+//     .text(function(d) { return d; });
