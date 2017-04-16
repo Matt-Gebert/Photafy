fe28db0b5ceb2525c878e46a9112ddc204a5f7d0
Matt Gebert <mattheuu.gebert@gmail.com>
2017-04-16 22:37:02 +1000

    Added Konami Code logger

--- Amy/amy.js
+++ Amy/amy.js
@@ -1,14 +1,36 @@
-
 //Well done if you've followed the paper trail here...
 //This is to hide from eyes other than amy's on Valentines Day!
 
-$(document).ready( function() {
-  var d = new Date(2017,2,14,0,0,0);
-  var d2 = new Date(2017,2,15,0,0,0);
-  if (Date((Date.now()) >> d) && (Date(Date.now()) << d2) {
+var track = [0,0,0,0,0,0,0,0,0,0];
+var konami = [38,38,40,40,37,39,37,39,66,65];
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    if ($.inArray(evt.keyCode), konami) {
+        track.shift();
+        track.push(evt.keyCode);
+        talkToAmy();
+    }
+};
+
+$(document).ready(function() {
+  //  var d = new Date(2017,2,14,0,0,0);
+  //  var d2 = new Date(2017,2,15,0,0,0);
+  //  if ((Date(Date.now()) >> d) && (Date(Date.now()) << d2)) {
+
+});
+
+function talkToAmy() {
+    var keyd = true;
+    for (var i=0; i < 10; i++) {
+        if (track[i] != konami[i])
+            keyd = false;
+    };
+    if (keyd) {
     $('.msg-para').html(''+
     "Hey Amy!<br />"+
-    "&nbsp&nbspHave a happy Valentines day haha :P Hope you and the family got back from Japan safely! <br />"+
+    "&nbsp&nbspHope you've had a happy Valentines day :P I trust you and the family got back from Japan safely!" +
+    "You give off a very enthusiastic vibe for new experiences, I'm looking forward to hearing about it!<br />"+
     "&nbsp&nbspSorry I'm not around atm haha, seems like we're both pretty busy but looking forward to catching "+
     "up again soon. I know we said we wouldn't do anything for valentines, because it's not really a big "+
     "fuss especially at the start of a dating period, but eh I can show some sort of initative haha :P<br />"+
@@ -19,4 +41,4 @@ $(document).ready( function() {
 
     $('#load_frame').delay(1000).animate({'opacity':0},2000,function(){$(this).css('display','none');});
   }
-});
+};
